a48ef7a3c25f6e8c364d7f541c02056919b747c2
Robencho <rubian.llanos.7mail.com>
2021-05-13 17:25:37 -0500

    Add Slider in home module.

--- app/src/main/java/com/rubio/movstore/utils/BindingAdapter.kt
+++ app/src/main/java/com/rubio/movstore/utils/BindingAdapter.kt
@@ -2,10 +2,12 @@ package com.rubio.movstore.utils
 
 import android.widget.ImageView
 import androidx.databinding.BindingAdapter
+import com.rubio.movstore.R
 import com.squareup.picasso.Picasso
 
 @BindingAdapter("showImgUrl")
 fun loadImgPicasso(view:ImageView, url:String?){
     Picasso.get().load(MovStoreConstants.URL_IMAGES.plus(url))
+        .placeholder(R.drawable.ic_splash)
         .into(view)
 }
